e6e7684b5f56f50782d9d212b2b6e18677070fa1
Tuấn Anh <hoangtuananh1772003@gmail.com>
2023-05-22 10:00:16 +0700

    wip: otp mail

---
+++ src/main/java/com/nhom7/foodg/controllers/ClientController.java
@@ -0,0 +1,24 @@
+package com.nhom7.foodg.controllers;
+
+import com.nhom7.foodg.services.ClientService;
+import com.nhom7.foodg.services.sdi.ClientSdi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping(value = "create")
+    public ResponseEntity<Boolean> create(
+            @RequestBody ClientSdi sdi
+    ) {
+        return ResponseEntity.ok(clientService.create(sdi));
+    }
+}
