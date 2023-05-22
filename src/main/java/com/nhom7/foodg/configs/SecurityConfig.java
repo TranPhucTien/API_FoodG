bc520c893bf72e45aac62ef454e8e93a8ad17900
thaidang111 <maisan73pnnh@gmail.com>
2023-05-22 09:25:35 +0700

    feat: Fix Post -> 403

--- src/main/java/com/nhom7/foodg/configs/SecurityConfig.java
+++ src/main/java/com/nhom7/foodg/configs/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -16,9 +21,20 @@ public class SecurityConfig {
                 .and().formLogin()
                 .and().logout().permitAll()
                 .and().authorizeRequests()
-                .requestMatchers("/admins/**", "/customers/**").hasRole("ADMIN")
-                .requestMatchers("/api/vnpay/**").hasRole("USER")
                 .anyRequest().permitAll();
+        httpSecurity.cors().and().csrf().disable();
         return httpSecurity.build();
     }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
